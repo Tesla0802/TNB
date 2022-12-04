9839c7206d788b375d9b81a0967523e270d29c42
lukasordia123 <99127921+lukasordia123@users.noreply.github.com>
2022-12-04 15:25:06 +0400

    .

--- card.js
+++ card.js
@@ -6,11 +6,11 @@ setTimeout(() => {
         <div class="card " style="display:flex ;" style="width: 18rem;">
         <img src="${element.data.imgSrc}" alt="">
         <div class="card-body">
-          <p class="card-text">${element.data.Fullname}</p>
-          <p class="card-text">${element.data.Profilename}</p>
-          <p class="card-text">${element.data.Profilelink}</p>
-          <p class="card-text">${element.data.Mail}</p>
-          <p class="card-text">${element.data.Contaction}</p>
+          <p class="card-text">Full Name - ${element.data.Fullname}</p>
+          <p class="card-text">Profile Name -  ${element.data.Profilename}</p>
+          <p class="card-text">Profile Link - ${element.data.Profilelink}</p>
+          <p class="card-text">Mail - ${element.data.Mail}</p>
+          <p class="card-text">Contact - ${element.data.Contaction}</p>
         </div>
       </div>
         `
