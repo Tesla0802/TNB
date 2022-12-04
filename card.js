21717a9d75099d712d6895977e49825a8281ec53
lukasordia123 <99127921+lukasordia123@users.noreply.github.com>
2022-12-04 15:20:44 +0400

    as

--- card.js
+++ card.js
@@ -3,7 +3,7 @@ let card = document.querySelector(".Center");
 setTimeout(() => {
     carArray.forEach((element) => {
         card.innerHTML += `
-        <div class="card " style="display:none ;" style="width: 18rem;">
+        <div class="card " style="display:flex ;" style="width: 18rem;">
         <img src="${element.data.imgSrc}" alt="">
         <div class="card-body">
           <p class="card-text">${element.data.Fullname}</p>
