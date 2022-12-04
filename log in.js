51c6040b6ad1306b6ae837730f00990a2d86e46d
lukasordia123 <99127921+lukasordia123@users.noreply.github.com>
2022-12-04 05:06:54 +0400

    Update log in.js

--- log in.js
+++ log in.js
@@ -1,1 +1,26 @@
-let email = document.querySelector("#email")
+let email = document.querySelector("#email");
+let password = document.querySelector("#password");
+const userArray = getArrayFromFirebase("User");
+
+submitButton.addEventListener("click", () => {
+  let email = emailInput.value;
+  let password = passwordInput.value;
+  let currentUser = {};
+  let successAuth = false;
+  userArray.forEach((element) => {
+    if (element.data.email === email && element.data.password === password) {
+      successAuth = true;
+      currentUser = element;
+      return;
+    }
+  });
+  if (!successAuth) {
+    displayAlert("შეცდომა", "არ არსებობს მომხარებელი", "info");
+    return;
+  }
+  displayAlert("შესრულდა", "წარმატებით დაემატა გაიარეთ ავტორიზაცია", "success");
+  localStorage.setItem("userid", currentUser.userid);
+  setTimeout(() => {
+    location.href = "index.html";
+  }, 1000);
+});
